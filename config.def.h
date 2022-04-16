e9b261f6982eac656c92f703e92cac3fe7c7356b
clawside <enyerplayus@gmail.com>
2022-04-15 20:44:58 -0400

    patches: Sticky patch
    Signed-off-by: clawside <enyerplayus@gmail.com>

--- config.def.h
+++ config.def.h
@@ -88,6 +88,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_r,      setlayout,      {.v = &layouts[4]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY,                       XK_s,      togglesticky,   {0} },
 	{ MODKEY,                       XK_Down,   moveresize,     {.v = "0x 25y 0w 0h" } },
 	{ MODKEY,                       XK_Up,     moveresize,     {.v = "0x -25y 0w 0h" } },
 	{ MODKEY,                       XK_Right,  moveresize,     {.v = "25x 0y 0w 0h" } },
